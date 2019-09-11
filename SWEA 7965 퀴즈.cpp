bc24473b540b8e8595d9e5b8e74a43ef7f0cf004
sminhyeok <sminhyuck@naver.com>
2019-09-11 13:24:49 +0900

    new

---
+++ SWEA 7965 퀴즈.cpp
@@ -0,0 +1,46 @@
+//2019.09.09 20:33~21:44
+#include <iostream>
+#define N 1000000
+using namespace std;
+
+unsigned long long DP[21][N+1];
+unsigned long long DP2[N + 1];
+
+void calc(int n) {
+	int index[21];	// 2진수 저장
+	int K = 0;
+	int temp = n;
+	while (temp) {
+		index[K++] = temp % 2;
+		temp /= 2;
+	}
+	DP2[n] = 1;
+	for (int i = K; i > 0; i--) {
+		if (index[i - 1]) {
+			DP2[n] = (DP2[n] * DP[i][n]) % 1000000007;
+		}
+	}
+}
+
+void DYNAMIC() {
+	DP2[1] = 1;
+	for (unsigned long long i = 2; i <= N; i++) {
+		DP[1][i] = i; int K = 1;
+		for (int j = 2; j <= i; j *= 2) {
+			DP[K + 1][i] = (DP[K][i] * DP[K][i]) % 1000000007; K++;
+		}
+		calc(i);
+		DP2[i] = (DP2[i] + DP2[i - 1]) % 1000000007;
+	}
+}
+
+int main() {
+	DYNAMIC();
+	int T;
+	cin >> T;
+	for (int t = 1; t <= T; t++) {
+		int n; cin >> n;
+		cout << "#" << t << " " << DP2[n] << endl;
+	}
+}
+
---
+++ SWEA 8425 님 게임.cpp
@@ -0,0 +1,3 @@
+//2019.09.09 20:12~
+#include <iostream>
+using namespace std;
